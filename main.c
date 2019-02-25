0ac80db2e6f5aeaa533b9cd0f96534cd04379b23
salomohutapea <hutapeasalomo@gmail.com>
2019-02-26 00:02:59 +0700

    This is the final program.

--- main.c
+++ main.c
@@ -9,29 +9,28 @@ float f(float x)
 
 float main()
 {
-    float a,b,c,d,e;
-    int count=1;
-    printf("\n\nMasukkan X0 dan X1:\n");
+    float a, b, c, e, rae;
+    int itr=1;
+
+    printf("\n\nEnter X0 and X1:\n");
     scanf("%f%f",&a,&b);
-    printf("Masukkan batas RAE:\n");
-    scanf("%f %d",&e);
+
+    printf("Enter allowed RAE:\n");
+    scanf("%f",&e);
+
     do
     {
-        if(f(a)==f(b)) {
-            printf("\nTidak ada solusi\n");
-            return 0;
-        }
         c=(a*f(b)-b*f(a))/(f(b)-f(a));
         a=b;
         b=c;
-        printf("\nIterasi ke-%d    x = %f\n",count,c);
-        printf("RAE pada iterasi ke-%d is %f", count, fabs(f(c)));
-        count++;
-        if(count==10000)
-        {
-            break;
-        }
+        rae = fabs((b - a) / b);
+        printf("\nIteration no-%d    x = %f\n",itr,c);
+        printf("RAE pada iterasi ke-%d is %f", itr, rae);
+        itr++;
         printf("\n");
-    } while(fabs(f(c)) > e);
-    printf("\nAfter %d iterations, root = %8.6f\n",count-1,c);
+    } while(rae > e);
+
+    printf("\nAfter %d iterations, root = %8.6f\n",itr-1,c);
+
+    return 0;
 }
