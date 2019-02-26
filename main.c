71e80442e081f568393f5af30a03aa8bffb95a22
salomohutapea <hutapeasalomo@gmail.com>
2019-02-26 10:13:12 +0700

    Delete time

--- main.c
+++ main.c
@@ -9,8 +9,6 @@ float f(float x)
 
 float main()
 {
-    double time_spent = 0.0;
-    clock_t begin = clock();
 
     float a = 10, b = 8, c, e = 0.01, rae;
     int itr=1;
@@ -29,8 +27,5 @@ float main()
 
     printf("\nAfter %d iterations, root = %8.6f\n",itr-1,c);
 
-    clock_t end = clock();
-    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("\nTime elpased is %f seconds", time_spent);
     return 0;
 }
